3c534bd6e720021c6cd49459cdfae1b24433a54b
lewis nyoike <nyoikelewis@gmail.com>
2017-07-14 11:36:30 +0300

    Defined a client method to acess data from the client table in order to form the one to many relationship

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -74,4 +74,16 @@ describe('Stylist') do
       expect(Stylist.find(test_stylist2.id)).to(eq(test_stylist2))
     end
   end
+
+  describe('#clients') do
+    it('returns an array of clients for that stylist') do
+      test_stylist = Stylist.new(name: 'Moringaschool stuff', id: nil)
+      test_stylist.save
+      test_client = Client.new(name: 'Latasha', stylist_id: test_stylist.id)
+      test_client.save
+      test_client2 = Client.new(name: 'Natasha', stylist_id: test_stylist.id)
+      test_client2.save
+      expect(test_stylist.clients).to(eq([test_client, test_client2]))
+    end
+  end
 end
