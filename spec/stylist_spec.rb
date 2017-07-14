1525056f26db14618173953b4bdb61d74e93b46f
lewis nyoike <nyoikelewis@gmail.com>
2017-07-14 10:43:16 +0300

    Wrote a name spec to check if name attribute is being fetched

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -7,4 +7,11 @@ describe('Stylist') do
       expect(stylist).to(be_an_instance_of(Stylist))
     end
   end
+
+  describe('#name') do
+    it('tells you its name') do
+      stylist = Stylist.new(name: 'mama nani', id: nil)
+      expect(stylist.name).to(eq('mama nani'))
+    end
+  end
 end
