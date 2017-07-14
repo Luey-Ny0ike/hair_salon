096eea3dfd38aa22d14e3a3d7a8ca9b759640d8b
lewis nyoike <nyoikelewis@gmail.com>
2017-07-14 11:13:07 +0300

    Defined a method to delete a stylist from the db

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -30,4 +30,8 @@ class Stylist
     @id = id
     DB.exec("UPDATE stylists SET name = '#{@name}' WHERE id = #{@id};")
   end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM stylists WHERE id = #{id};")
+  end
 end
