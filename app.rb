817ed30b223946cee822265a80b4ac33102ffc73
lewis nyoike <nyoikelewis@gmail.com>
2017-07-17 10:36:36 +0300

    Connected it to the local db and called the .all method to return all the data in the db

--- app.rb
+++ app.rb
@@ -5,3 +5,9 @@ require('./lib/client')
 require('./lib/stylist')
 require('pg')
 
+DB = PG.connect(dbname: 'hair_salon')
+# connetced it to the database
+get('/') do
+  @stylists = Stylist.all
+  erb(:index)
+end
